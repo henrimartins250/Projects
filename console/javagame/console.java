8df94e1d7075ee824e313926ac90c1112858c447
henrique <henrinkcstd@gmail.com>
2025-04-02 07:52:28 -0300

    Create console.java

---
+++ console/javagame/console.java
@@ -0,0 +1,16 @@
+
+public class console {
+    public static void main(String args[]){
+        generator randpicker = new generator();
+        System.out.println("favorrite fruit: " + randpicker.FruitPicker());
+        System.out.println("name: " + randpicker.PersonPicker());
+        System.out.println("Job: " + randpicker.JobPicker());
+        System.out.println("favorite color: " + randpicker.ColorPicker());
+    
+
+//     game loop
+        while (true) { 
+            
+        }
+    }    
+}

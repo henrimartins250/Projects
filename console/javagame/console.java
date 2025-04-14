762dd4773b1a40e33561cbd0dede16ad67af981b
henrique <henrinkcstd@gmail.com>
2025-04-13 21:49:00 -0300

    Update console.java

--- console/javagame/console.java
+++ console/javagame/console.java
@@ -2,10 +2,10 @@
 public class console {
     public static void main(String args[]){
         generator randpicker = new generator();
-        System.out.println("favorrite fruit: " + randpicker.FruitPicker());
-        System.out.println("name: " + randpicker.PersonPicker());
-        System.out.println("Job: " + randpicker.JobPicker());
-        System.out.println("favorite color: " + randpicker.ColorPicker());
+        System.out.println("FavorRrite fruit: " + randpicker.FruitPicker());
+        System.out.println("noname: " + randpicker.PersonPicker());
+        System.out.println("blowJob: " + randpicker.JobPicker());
+        System.out.println("Favorite color: " + randpicker.ColorPicker());
     
 
 //     game loop
